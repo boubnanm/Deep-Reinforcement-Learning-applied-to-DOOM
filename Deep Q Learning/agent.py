16220b2bd8e9cb9f6c2c63ecc6a57e61c275b184
Ayman Chaouki <chaoukiayman2@gmail.com>
2019-01-27 12:29:46 +0000

    Modifying tensorboard logdir for Deep Q Learning

---
+++ Deep Q Learning/agent.py
@@ -0,0 +1,276 @@
+from utils import *
+from memory import *
+from models import *
+from collections import namedtuple
+from tensorboardX import SummaryWriter
+
+Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state', 'dones'))
+
+class Agent:
+    
+    def __init__(self, possible_actions, scenario, memory = 'uniform', max_size = 1000, stack_size = 4, batch_size = 64, resize = (120, 160)):
+        """
+        Description
+        --------------
+        Constructor of Agent class.
+        
+        Attributes
+        --------------
+        possible_actions : List, contains the one-hot encoded possible actions to take for the agent.
+        scenario         : String, either 'basic' or 'deadly_corridor'
+        memory           : String with values in ['uniform', 'prioritized'] depending on the type of replay memory to be used     (defaul='uniform')
+        max_size         : Int, maximum size of the replay buffer (default=1000)
+        stack_size       : Int, the number of frames to stack to create motion (default=4)
+        batch_size       : Int, the batch size used for backpropagation (default=64)
+        resize           : tuple, shape of the resized frame (default=(120,160))
+        """
+        
+        if memory == 'uniform':
+            self.memory = MemoryUniform(max_size)
+            
+        elif memory == 'prioritized':
+            self.memory = MemoryPrioritized(max_size)
+            
+        self.memory_type = memory
+        self.stack_size = stack_size
+        self.possible_actions = possible_actions
+        self.scenario = scenario
+        self.batch_size = batch_size
+        self.resize = resize
+        
+    def get_reward(self, variables_cur, variables_prev):
+        """
+        Description
+        --------------
+        Reward reshaping
+        
+        Parameters
+        --------------
+        variables_cur  : dict, dictionnary containing current variables (kills, health and ammo.
+        variables_prev : dict, dictionnary containing previous variables (kills, health and ammo.
+        
+        Returns
+        --------------
+        Float, a reshaped reward.
+        """
+        
+        r = 0
+        if self.scenario == 'defend_the_center':
+            r += variables_cur['kills'] - variables_prev['kills']
+            if variables_cur['ammo'] < variables_prev['ammo']:
+                r -= 0.1
+
+            if variables_cur['health'] < variables_prev['health']:
+                r -= 0.1
+                
+        elif self.scenario == 'deadly_corridor':
+            r += (variables_cur['kills'] - variables_prev['kills'])*5
+            if variables_cur['ammo'] < variables_prev['ammo']:
+                r -= 0.1
+
+            if variables_cur['health'] < variables_prev['health']:
+                r -= 1
+                
+        return r
+        
+            
+    def train(self, game, total_episodes = 100, pretrain = 100, frame_skip = 4, enhance = 'none', lr = 1e-4, max_tau = 100, 
+                     explore_start = 1.0, explore_stop = 0.01, decay_rate = 0.0001, gamma = 0.99, freq = 50, init_zeros = False):
+        """
+        Description
+        --------------
+        Unroll trajectories to gather experiences and train the model.
+        
+        Parameters
+        --------------
+        game               : VizDoom game instance.
+        total_episodes     : Int, the number of training episodes (default=100)
+        pretrain           : Int, the number of initial experiences to put in the replay buffer (default=100)
+        frame_skip         : Int, the number of frames to repeat the action on (default=4)
+        enhance            : String in ['none', 'dueling'] (default='none')
+        lr                 : Float, the learning rate (default=1e-4)
+        max_tau            : Int, number of steps to performe double q-learning parameters update (default=100)
+        explore_start      : Float, the initial exploration probaboility (default=1.0)
+        explore_stop       : Float, the final exploration probability (default=0.01)
+        decay_rate         : Float, the decay rate of the exploration probability (default=1e-3)
+        gamma              : Float, the reward discoundting coefficient, should be between 0 and 1 (default=0.99)
+        freq               : Int, number of episodes to save model weights (default=50)
+        init_zeros         : Boolean, whether to initialize the weights to zero or not.
+        """
+        
+        # Setting tensorboadX and variables of interest
+        writer = SummaryWriter(log_dir = 'runs/' + self.scenario)
+        kill_count = np.zeros(10) # This list will contain kill counts of each 10 episodes in order to compute moving average
+        ammo = np.zeros(10) # This list will contain ammo of each 10 episodes in order to compute moving average
+        rewards = np.zeros(10) # This list will contain rewards of each 10 episodes in order to compute moving average
+        losses = np.zeros(10) # This list will contain losses of each 10 episodes in order to compute moving average
+        # Pretraining phase
+        game.new_episode()
+        # Initialize current and previous game variables dictionnaries
+        variables_cur = {'kills' : game.get_game_variable(KILLCOUNT), 'health' : game.get_game_variable(HEALTH), 
+                        'ammo' : game.get_game_variable(AMMO2)}
+        variables_prev = variables_cur.copy()
+        # Get 1st state
+        state = get_state(game)
+        stacked_frames = deque([torch.zeros(self.resize, dtype=torch.int) for i in range(self.stack_size)], maxlen = self.stack_size)
+        state, stacked_frames = stack_frames(stacked_frames, state, True, self.stack_size, self.resize)
+        for i in range(pretrain):
+            # Get action and reward
+            action = random.choice(self.possible_actions)
+            reward = game.make_action(action, frame_skip)
+            # Update the game vaiables dictionnaries and get the reshaped reward
+            variables_cur['kills'] = game.get_game_variable(KILLCOUNT)
+            variables_cur['health'] = game.get_game_variable(HEALTH)
+            variables_cur['ammo'] = game.get_game_variable(AMMO2)
+            reward += self.get_reward(variables_cur, variables_prev)
+            variables_prev = variables_cur.copy()
+            # Put reward and action in tensor form
+            reward = torch.tensor([reward], dtype = torch.float)
+            action = torch.tensor([action], dtype = torch.float)
+            done = game.is_episode_finished()
+            if done:
+                # Set next state to zeros
+                next_state = np.zeros((240, 320), dtype='uint8')[:, :, None] # (240, 320) is the screen resolution, see cfg files /scenarios
+                next_state, stacked_frames = stack_frames(stacked_frames, next_state, False, self.stack_size, self.resize)
+                # Add experience to replay buffer
+                self.memory.add((state, action, reward, next_state, torch.tensor([not done], dtype = torch.float)))
+                # Start a new episode
+                game.new_episode()
+                state = get_state(game)
+                state, stacked_frames = stack_frames(stacked_frames, state, True, self.stack_size, self.resize)
+
+            else:
+                # Get next state
+                next_state = get_state(game)
+                next_state, stacked_frames = stack_frames(stacked_frames, next_state, False, self.stack_size, self.resize)
+                # Add experience to memory
+                self.memory.add((state, action, reward, next_state, torch.tensor([not done], dtype = torch.float)))
+                # update state variable
+                state = next_state
+        
+        # Exploration-Exploitation phase
+        decay_step = 0
+        if enhance == 'none':
+            dqn_model = DQNetwork(init_zeros = init_zeros, out = len(self.possible_actions))
+            target_dqn_model = DQNetwork(init_zeros = init_zeros, out = len(self.possible_actions))
+            if use_cuda:
+                print("End of trainig phase: The screen might be frozen now, don't worry, models take some time to be loaded on GPU")
+                dqn_model.cuda()
+                target_dqn_model.cuda()
+
+        elif enhance == 'dueling':
+            dqn_model = DDDQNetwork(init_zeros = init_zeros, out = len(self.possible_actions))
+            target_dqn_model = DDDQNetwork(init_zeros = init_zeros, out = len(self.possible_actions))
+            if use_cuda:
+                print("End of trainig phase: The screen might be frozen now, don't worry, models take some time to be loaded on GPU")
+                dqn_model.cuda()
+                target_dqn_model.cuda()
+
+        optimizer = optim.Adam(dqn_model.parameters(), lr=lr)
+        for episode in range(total_episodes):
+            # When tau > max_tau perform double q-learning update.
+            tau = 0
+            episode_rewards = []
+            game.new_episode()
+            variables_cur = {'kills' : game.get_game_variable(KILLCOUNT), 'health' : game.get_game_variable(HEALTH), 
+                            'ammo' : game.get_game_variable(AMMO2)}
+            variables_prev = variables_cur.copy()
+            # Get 1st state
+            done = game.is_episode_finished()
+            state = get_state(game)
+            stacked_frames = deque([torch.zeros(self.resize, dtype=torch.int) for i in range(self.stack_size)], maxlen = self.stack_size)
+            state, stacked_frames = stack_frames(stacked_frames, state, True, self.stack_size, self.resize)
+            while (not done):
+                tau += 1
+                decay_step += 1
+                # Predict the action to take
+                action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, dqn_model, self.possible_actions)
+                # Perform the chosen action on frame_skip frames
+                reward = game.make_action(action, frame_skip)
+                # Update the game vaiables dictionnaries and get the reshaped reward
+                variables_cur['kills'] = game.get_game_variable(KILLCOUNT)
+                variables_cur['health'] = game.get_game_variable(HEALTH)
+                variables_cur['ammo'] = game.get_game_variable(AMMO2)
+                reward += self.get_reward(variables_cur, variables_prev)
+                variables_prev = variables_cur.copy()
+                # Check if the episode is done
+                done = game.is_episode_finished()
+                # Add the reward to total reward
+                episode_rewards.append(reward)
+                reward = torch.tensor([reward], dtype = torch.float)
+                action = torch.tensor([action], dtype = torch.float)
+                if done:
+                    next_state = np.zeros((240, 320), dtype='uint8')[:, :, None]
+                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False, self.stack_size, self.resize)
+                    total_reward = np.sum(episode_rewards)
+                    print('Episode: {}'.format(episode),
+                              'Total reward: {:.2f}'.format(total_reward),
+                              'Training loss: {:.4f}'.format(loss),
+                              'Explore P: {:.4f}'.format(explore_probability))
+                    # Add experience to the replay buffer
+                    self.memory.add((state, action, reward, next_state, torch.tensor([not done], dtype = torch.float)))
+                    # Add number of kills and ammo variables
+                    kill_count[episode%10] = game.get_game_variable(KILLCOUNT)
+                    ammo[episode%10] = game.get_game_variable(AMMO2)
+                    rewards[episode%10] = total_reward
+                    losses[episode%10] = loss
+                    # Update writer
+                    if (episode > 0) and (episode%10 == 0):
+                        writer.add_scalar('Game variables/Kills', kill_count.mean(), episode)
+                        writer.add_scalar('Game variables/Ammo', ammo.mean(), episode)
+                        writer.add_scalar('Reward Loss/Reward', rewards.mean(), episode)
+                        writer.add_scalar('Reward Loss/loss', losses.mean(), episode)
+
+                else:
+                    # Get the next state
+                    next_state = get_state(game)
+                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False, self.stack_size, self.resize)
+                    # Add experience to memory
+                    self.memory.add((state, action, reward, next_state, torch.tensor([not done], dtype = torch.float)))
+                    # Update state variable
+                    state = next_state
+
+                # Learning phase
+                if self.memory_type == 'uniform':
+                    transitions = self.memory.sample(self.batch_size)
+                    batch = Transition(*zip(*transitions))
+                    states_mb = torch.cat(batch.state)
+                    actions_mb = torch.cat(batch.action)
+                    rewards_mb = torch.cat(batch.reward)
+                    next_states_mb = torch.cat(batch.next_state)
+                    dones_mb = torch.cat(batch.dones)
+                    if torch.cuda.is_available(): # Then use GPU device
+                        next_states_mb = next_states_mb.cuda()
+                        states_mb = states_mb.cuda()
+                        q_next_state = dqn_model(next_states_mb).cpu()
+                        q_target_next_state = target_dqn_model(next_states_mb).cpu()
+                        q_state = dqn_model(states_mb).cpu()
+
+                    else: # Then use CPU device
+                        q_next_state = dqn_model.forward(next_states_mb)
+                        q_target_next_state = target_dqn_model.forward(next_states_mb)
+                        q_state = dqn_model.forward(states_mb)
+
+                    targets_mb = rewards_mb + (gamma*dones_mb*torch.max(q_target_next_state, 1)[0])
+                    output = (q_state * actions_mb).sum(1)
+                    loss = F.mse_loss(output, targets_mb)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                if tau > max_tau:
+                    # Update the parameters of our target_dqn_model with DQN_weights
+                    update_target(dqn_model, target_dqn_model)
+                    print('model updated')
+                    tau = 0
+                    
+            if (episode % freq) == 0: # +1 just to avoid the conditon episode != 0
+                model_file = 'weights/' + self.scenario + '/' + enhance + '_' + str(episode) + '.pth'
+                torch.save(dqn_model.state_dict(), model_file)
+                print('\nSaved model to ' + model_file)
+        
+        writer.export_scalars_to_json("./all_scalars.json")
+        writer.close()
+                    
+                    
+                    
---
+++ Deep Q Learning/utils.py
@@ -0,0 +1,364 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as T
+
+import numpy as np
+import random
+import time
+from vizdoom import *
+from models import *
+
+from collections import deque
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Boolean specifying whether GPUs are available or not.
+use_cuda = torch.cuda.is_available()
+
+"""
+Environment tools
+"""
+
+def create_environment(scenario = 'basic', window = False):
+    """
+    Description
+    ---------------
+    Creates VizDoom game instance along with some predefined possible actions.
+    
+    Parameters
+    ---------------
+    scenario : String, either 'basic' or 'deadly_corridor' or 'defend_the_center', the Doom scenario to use (default='basic')
+    window   : Boolean, whether to render the window of the game or not (default=False)
+    
+    Returns
+    ---------------
+    game             : VizDoom game instance.
+    possible_actions : List, the one-hot encoded possible actions.
+    """
+    
+    game = DoomGame()
+    if window:
+        game.set_window_visible(True)
+        
+    else:
+        game.set_window_visible(False)
+    
+    # Load the correct configuration
+    if scenario == 'basic':
+        game.load_config("scenarios/basic.cfg")
+        game.set_doom_scenario_path("scenarios/basic.wad")
+        game.init()
+        left = [1, 0, 0]
+        right = [0, 1, 0]
+        shoot = [0, 0, 1]
+        possible_actions = [left, right, shoot]
+        
+    elif scenario == 'deadly_corridor':
+        game.load_config("scenarios/deadly_corridor.cfg")
+        game.set_doom_scenario_path("scenarios/deadly_corridor.wad")
+        game.init()
+        possible_actions = np.identity(6,dtype=int).tolist()
+#         possible_actions.extend([[0, 0, 1, 0, 1, 0], [0, 0, 1, 0, 0, 1]]) # Composed actions need some work
+        
+    elif scenario == 'defend_the_center':
+        game.load_config("scenarios/defend_the_center.cfg")
+        game.set_doom_scenario_path("scenarios/defend_the_center.wad")
+        game.init()
+        left = [1, 0, 0]
+        right = [0, 1, 0]
+        shoot = [0, 0, 1]
+        possible_actions = [left, right, shoot]
+#         possible_actions.extend([[1, 0, 1], [0, 1, 1]]) # Composed actions need some work
+    
+    return game, possible_actions
+       
+
+def test_environment(weights, scenario = 'basic', window = False, total_episodes = 100, enhance = 'none', frame_skip = 2):
+    """
+    Description
+    ---------------
+    Test a trained agent in a scenario (Be careful, the chosen weights must match the training scenario)
+    
+    Parameters
+    ---------------
+    weights        : String, path to .pth file containing the weights of the network we want to test.
+    scenario       : String, either 'basic' or 'deadly_corridor', the Doom scenario to use (default='basic')
+    window         : Boolean, whether to render the window of the game or not (default=False)
+    total_episodes : Int, the number of testing episodes (default=100)
+    enhance        : String, 'none' or 'dueling' (default='none')
+    frame_skip     : Int, the number of frames to repeat the action on (default=2)
+    
+    Returns
+    ---------------
+    game             : VizDoom game instance.
+    possible_actions : List, the one-hot encoded possible actions.
+    """
+    
+    game = DoomGame()
+    if window:
+        game.set_window_visible(True)
+        
+    else:
+        game.set_window_visible(False)
+    
+    # Load the correct configuration
+    if scenario == 'basic':
+        game.load_config("scenarios/basic.cfg")
+        game.set_doom_scenario_path("scenarios/basic.wad")
+        game.init()
+        left = [1, 0, 0]
+        right = [0, 1, 0]
+        shoot = [0, 0, 1]
+        possible_actions = [left, right, shoot]
+        
+    elif scenario == 'deadly_corridor':
+        game.load_config("scenarios/deadly_corridor.cfg")
+        game.set_doom_scenario_path("scenarios/deadly_corridor.wad")
+        game.init()
+        possible_actions = np.identity(6,dtype=int).tolist()
+#         possible_actions.extend([[0, 0, 1, 0, 1, 0], [0, 0, 1, 0, 0, 1]])
+
+    elif scenario == 'defend_the_center':
+        game.load_config("scenarios/defend_the_center.cfg")
+        game.set_doom_scenario_path("scenarios/defend_the_center.wad")
+        game.init()
+        left = [1, 0, 0]
+        right = [0, 1, 0]
+        shoot = [0, 0, 1]
+        possible_actions = [left, right, shoot]
+
+    if enhance == 'none':
+        model = DQNetwork(out = len(possible_actions))
+        if use_cuda:
+            model.cuda()
+
+    elif enhance == 'dueling':
+        model = DDDQNetwork(out = len(possible_actions))
+        if use_cuda:
+            model.cuda()
+
+    # Load the weights of the model
+    state_dict = torch.load(weights)
+    model.load_state_dict(state_dict)
+    for i in range(total_episodes):
+        game.new_episode()
+        done = game.is_episode_finished()
+        state = get_state(game)
+        in_channels = model._modules['conv_1'].in_channels
+        stacked_frames = deque([torch.zeros((120, 160), dtype=torch.int) for i in range(in_channels)], maxlen = in_channels)
+        state, stacked_frames = stack_frames(stacked_frames, state, True, in_channels)
+        while not done:
+            if use_cuda:
+                q = model(state.cuda())
+
+            else:
+                q = model(state)
+
+            action = possible_actions[int(torch.max(q, 1)[1][0])]
+            reward = game.make_action(action, frame_skip)
+            done = game.is_episode_finished()
+            time.sleep(0.02)
+            
+        print ("Total reward:", game.get_total_reward())
+        time.sleep(2)
+        
+    game.close()
+
+"""
+Preproessing tools
+"""
+def get_state(game):
+    """
+    Description
+    --------------
+    Get the current state from the game.
+    
+    Parameters
+    --------------
+    game : VizDoom game instance.
+    
+    Returns
+    --------------
+    state : 4-D Tensor, we add the temporal dimension.
+    """
+    
+    state = game.get_state().screen_buffer
+    return state[:, :, None] 
+
+# class Crop(object):
+#     """Crops the given PIL.Image using given pixel coordinates.
+
+#     Args:
+#         i – int, Upper pixel coordinate.
+#         j – int, Left pixel coordinate.
+#         h – int, Height of the cropped image.
+#         w – int, Width of the cropped image.
+#     """
+
+#     def __init__(self, i, j, h, w):
+#         self.i = i
+#         self.j = j
+#         self.h = h
+#         self.w = w
+
+#     def __call__(self, img):
+#         """
+#         Args:
+#             img (PIL.Image): Image to be cropped.
+
+#         Returns:
+#             PIL.Image: Cropped image.
+#         """
+#         return img.crop((self.i, self.h, self.j, self.w))
+    
+# def transforms(crop = False, coords = (30, 300, 60, 180), resize = (120, 160)):
+#     """
+#     Description
+#     -------------
+#     Preprocess image screen before feeding it to a neural network.
+    
+#     Parameters
+#     -------------
+#     crop   : boolean, whether to crop or not (default=False)
+#     coords : tuple, when crop is True, the coordinates to apply cropping (default=(30, 300, 60, 180)).
+#              Be careful to the value of this parameter with respect to the screen resolution.
+#     resize : tuple, shape of the resized frame (default=(120,160))
+    
+#     Returns
+#     -------------
+#     torchvision.transforms.transforms.Compose object, the composed transformations.
+#     """
+    
+#     if crop:
+#         return T.Compose([T.ToPILImage(),
+#                     Crop(coords[0], coords[1], coords[2], coords[3]),
+#                     T.Resize(resize),
+#                     T.ToTensor()])
+        
+#     else:
+#         return T.Compose([T.ToPILImage(),
+#                     T.Resize(resize),
+#                     T.ToTensor()])
+
+def transforms(resize = (120, 160)):
+    """
+    Description
+    -------------
+    Preprocess image screen before feeding it to a neural network.
+    
+    Parameters
+    -------------
+    resize : tuple, shape of the resized frame (default=(120,160))
+    
+    Returns
+    -------------
+    torchvision.transforms.transforms.Compose object, the composed transformations.
+    """
+    
+    return T.Compose([T.ToPILImage(),
+                T.Resize(resize),
+                T.ToTensor()])
+    
+def stack_frames(stacked_frames, state, is_new_episode, maxlen = 4, resize = (120, 160)):
+    """
+    Description
+    --------------
+    Stack multiple frames to create a notion of motion in the state.
+    
+    Parameters
+    --------------
+    stacked_frames : collections.deque object of maximum length maxlen.
+    state          : the return of get_state() function.
+    is_new_episode : boolean, if it's a new episode, we stack the same initial state maxlen times.
+    maxlen         : Int, maximum length of stacked_frames (default=4)
+    resize         : tuple, shape of the resized frame (default=(120,160))
+    
+    Returns
+    --------------
+    stacked_state  : 4-D Tensor, same information as stacked_frames but in tensor. This represents a state.
+    stacked_frames : the updated stacked_frames deque.
+    """
+    
+    # Preprocess frame
+    frame = transforms(resize)(state)
+    if is_new_episode:
+        # Clear our stacked_frames
+        stacked_frames = deque([frame[None] for i in range(maxlen)], maxlen=maxlen) # We add a dimension for the batch
+        # Stack the frames
+        stacked_state = torch.cat(tuple(stacked_frames), dim = 1)
+        
+    else:
+        # Append frame to deque, automatically removes the oldest frame
+        stacked_frames.append(frame[None]) # We add a dimension for the batch
+        # Build the stacked state (first dimension specifies different frames)
+        stacked_state = torch.cat(tuple(stacked_frames), dim = 1)
+    
+    return stacked_state, stacked_frames
+
+
+"""
+epsilon-greedy
+"""
+def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, model, possible_actions):
+    """
+    Description
+    -------------
+    Epsilon-greedy policy
+    
+    Parameters
+    -------------
+    explore_start    : Float, the initial exploration probability.
+    explore_stop     : Float, the last exploration probability.
+    decay_rate       : Float, the rate at which the exploration probability decays.
+    state            : 4D-tensor (batch, motion, image)
+    model            : models.DQNetwork or models.DDDQNetwork object, the architecture used.
+    possible_actions : List, the one-hot encoded possible actions.
+    
+    Returns
+    -------------
+    action              : np.array of shape (number_actions,), the action chosen by the greedy policy.
+    explore_probability : Float, the exploration probability.
+    """
+    
+    exp_exp_tradeoff = np.random.rand()
+    explore_probability = explore_stop + (explore_start - explore_stop)*np.exp(-decay_rate*decay_step)
+    if (explore_probability > exp_exp_tradeoff):
+        action = random.choice(possible_actions)
+        
+    else:
+        if use_cuda:
+            Qs = model.forward(state.cuda())
+            
+        else:
+            Qs = model.forward(state)
+            
+        action = possible_actions[int(torch.max(Qs, 1)[1][0])]
+
+    return action, explore_probability
+
+"""
+Double Q-learning tools
+"""
+def update_target(current_model, target_model):
+    """
+    Description
+    -------------
+    Update the parameters of target_model with those of current_model
+    
+    Parameters
+    -------------
+    current_model, target_model : torch models
+    """
+    target_model.load_state_dict(current_model.state_dict())
+
+
+
+
+
+
+
+
